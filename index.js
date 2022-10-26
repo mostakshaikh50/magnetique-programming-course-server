bd5b4aa2ad25d71e5713d6dc8c01fba2e2dcf1de
mostakshaikh50 <mostak.50.cse@gmail.com>
2022-10-26 01:41:48 +0100

    install cors

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-const categories = require('./data/categories.json');
-//const news = require('./data/news.json');
+const courses = require('./data/categories.json');
+const courseDetails = require('./data/category_details.json');
 
 app.use(cors());
 
@@ -12,13 +12,16 @@ app.get('/', (req, res) =>{
     res.send('News API Running');
 });
 
-app.get('/courses-categories', (req, res) =>{
-    res.send(categories);
+app.get('/courses', (req, res) =>{
+    res.send(courses);
 })
 
-// app.get('/news', (req, res) =>{
-//     res.send(news);
-// })
+app.get('/coursedetails/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedCourse = courseDetails.find( n => n._id === id);
+    res.send(selectedCourse);
+   
+})
 
 
 app.listen(port, () =>{
